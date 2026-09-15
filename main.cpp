e5494b1bcce0e5b49bf892731e1c3c74dd89b173
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-09-16 00:18:05 +0300

    Add clicker class and base types

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
 
-int main()
-{
+class Clicker {
+public:
+  Clicker() :
+    start_(std::chrono::steady_clock::now())
+  {}
 
+  double millisec() const {
+    using std::chrono::duration_cast;
+    using std::chrono::milliseconds;
+    const auto t = std::chrono::steady_clock::now();
+    return static_cast<double>(duration_cast<milliseconds>(t - start_).count());
+  }
+
+private:
+  std::chrono::steady_clock::time_point start_;
+};
+
+using data_t = std::vector<unsigned long long>;
+using value_t = data_t::value_type;
+
+int main() {
+  return 0;
 }

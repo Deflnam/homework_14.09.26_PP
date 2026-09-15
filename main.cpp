e935e1167903e35e8fbc3b34a1425b38f9f96456
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-09-16 00:24:06 +0300

    add parallel sum

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <future>
+#include <exception>
 
 class Clicker {
 public:
@@ -30,6 +32,62 @@ value_t workerSum(const data_t& data, size_t start, size_t end) {
   return sum;
 }
 
-int main() {
+value_t calculateParallel(const data_t& data, size_t threadCount) {
+  const size_t baseSize = data.size() / threadCount;
+  const size_t remainder = data.size() % threadCount;
+
+  std::vector<std::future<value_t>> futures;
+  futures.reserve(threadCount);
+
+  size_t currStart = 0;
+  for (size_t i = 0; i < threadCount; ++i) {
+    const size_t currentSize = baseSize + (i < remainder ? 1 : 0);
+    const size_t currEnd = currStart + currentSize;
+
+    futures.push_back(
+      std::async(std::launch::async, workerSum, std::cref(data), currStart, currEnd)
+    );
+
+    currStart = currEnd;
+  }
+
+  value_t totalSum = 0;
+  for (size_t i = 0; i < threadCount; ++i) {
+    totalSum += futures[i].get();
+  }
+
+  return totalSum;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Error: please enter thread count as argument\n";
+    return 1;
+  }
+
+  size_t threadCount = 0;
+  try {
+    threadCount = std::stoull(argv[1]);
+  } catch (const std::exception& e) {
+    std::cerr << "Error of parsing argument: " << e.what() << "\n";
+    return 1;
+  }
+
+  if (threadCount == 0) {
+    std::cerr << "Error: thread count must be more than zero\n";
+    return 1;
+  }
+
+  constexpr size_t size = 100000000;
+  const data_t values(size, 1);
+
+  Clicker cl;
+  const value_t result = calculateParallel(values, threadCount);
+  const double total = cl.millisec();
+
+  std::cout << "Threads: " << threadCount
+            << "\nSum: " << result
+            << "\nTime: " << total << " ms\n";
+
   return 0;
 }
